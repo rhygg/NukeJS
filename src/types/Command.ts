fc8030e21dfa074849cb09b230b657b1c641c6a9
LostNuke <nroostaazad@gmail.com>
2021-01-11 17:58:38 +0100

    Added license headers

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,3 +1,24 @@
+/**
+ * 
+ *   NukeJS -- A Discord Bot Framework
+ *   Copyright (C) 2021 Nikan Roosta Azad
+ * 
+ *                  This file is part of NukeJS.
+ *
+ *   NukeJS is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   NukeJS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NukeJS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { BitFieldResolvable, PermissionString } from "discord.js";
 
 interface CommandOptions {
